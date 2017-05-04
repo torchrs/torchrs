91ad8d380c418043d442bc2fe8d34a1bba5549b5
Matt Macy <mmacy@torch.rs>
2017-05-03 20:53:40 -0700

    add first pass at Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,25 @@
 extern crate num;
+extern crate rand;
 extern crate rutorch;
 
 use rutorch::*;
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
+use rand::Rng;
 //use std::convert::From;
-//use num::Num;
+use num::Num;
 use std::cmp::max;
 
+struct BaseIter<'a, T, P: 'a + Index<isize>> {
+	idx: usize,
+	parent: &'a P,
+	t: std::marker::PhantomData<T>,
+}
+
+impl <'a, T, P: Index<isize> >BaseIter<'a, T, P> {
+	fn new(parent: &'a P) -> Self {
+		BaseIter { idx: 0, parent: parent, t: std::default::Default::default()}
+	}
+}
 
 trait Storage {
 	fn new() -> Self;
@@ -26,11 +39,35 @@ impl Storage for FloatStorage {
 	}
 }
 
+impl FloatStorage {
+	fn iter(&mut self) -> BaseIter<f32, FloatStorage> {
+		BaseIter::new(&self)
+	}
+}
+
 impl Index<isize> for FloatStorage {
 	type Output = f32;
 
 	fn index(&self, idx: isize) -> &f32 {
 		unsafe {&*(*self.t).data.offset(idx)}
+	}
+}
+
+impl IndexMut<isize> for FloatStorage {
+	fn index_mut(&mut self, idx: isize) -> &mut f32 {
+		unsafe {&mut *(*self.t).data.offset(idx)}
+	}
+}
+
+impl <'a, f32, FloatStorage >Iterator for BaseIter<'a, f32, FloatStorage> {
+	type Item = f32;
+
+	fn next(&mut self) -> Option<f32> {
+		let idx = self.idx;
+		// XXX check length ---
+		let ret : f32 = (*self.parent).index(idx as isize);
+		self.idx += 1;
+		Some(ret)
 	}
 }
 
@@ -64,7 +101,9 @@ impl <'a>Tensor<'a> for FloatTensor {
 	}
 	fn randn(dims: &'a [isize]) -> Self {
 		/* XXX */
-		Tensor::sized(dims)
+		let t = Tensor::sized(dims);
+//		x = random::<f32>();
+		t
 	}
 }
 
@@ -84,6 +123,22 @@ impl <'a> Index<&'a [isize]> for FloatTensor {
 		&self.storage[index]
 	}
 }
+
+impl <'a> IndexMut<&'a [isize]> for FloatTensor {
+	fn index_mut(&mut self, idx: &'a [isize]) -> &mut f32 {
+		let mut index : isize = 0;
+		let lastidx = max(0, idx.len() as isize - 1) as usize;
+		if idx.len() != self.dims.len() { panic!("bad dimlen")}
+		for i in 0..lastidx {
+			if idx[i] >= self.dims[i] { panic!("bad dimlen")}
+			index += idx[i] * self.dims[i]
+		}
+		if idx[lastidx] >= self.dims[lastidx] { panic!("bad dimlen")}
+		index += idx[lastidx];
+		&mut self.storage[index]
+	}
+}
+
 
 #[cfg(test)]
 mod tests {
