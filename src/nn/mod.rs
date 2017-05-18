e55e09362096bcc933835bdc0c3691ccc4401d03
mmacy <mmacy@torch.rs>
2017-05-17 20:47:46 -0700

    further flesh out module interface

--- src/nn/mod.rs
+++ src/nn/mod.rs
@@ -22,17 +22,22 @@ impl<'a> Parameter<'a> {
 pub struct Module<'a> {
     pub _name: &'a str,
     _backend: TorchBackend,
-    //	_buffers: HashTable<&str, Tensor>
+//    _root: *mut ModIntf<'a>,
+
+    	_buffers: HashMap<&'a str, &'a mut Tensor>,
     //	_backward_hooks:
     //	_forward_hooks:
-    //_modulesp: LinkedHashMap<&'a str, *mut Module<'a>>,
     _params: HashMap<&'a str, *mut Parameter<'a>>,
-    _modulesp: HashMap<&'a str, *mut Module<'a>>,
+    _modules: HashMap<&'a str, *mut Module<'a>>,
     training: bool,
 }
 pub struct PtrIterMut<'a, T: 'a> {
     //mod_iter: linked_hash_map::IterMut<'a, &'a str, *mut T>,
     mod_iter: hash_map::IterMut<'a, &'a str, *mut T>,
+}
+pub struct PtrIter<'a, T: 'a> {
+    //mod_iter: linked_hash_map::IterMut<'a, &'a str, *mut T>,
+    mod_iter: hash_map::Iter<'a, &'a str, *mut T>,
 }
 
 impl<'a, T> Iterator for PtrIterMut<'a, T> {
@@ -46,16 +51,28 @@ impl<'a, T> Iterator for PtrIterMut<'a, T> {
     }
 }
 
+impl<'a, T> Iterator for PtrIter<'a, T> {
+    type Item = (&'a str, &'a T);
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some((name, t)) = self.mod_iter.next() {
+            Some((name, unsafe { & **t as & T }))
+        } else {
+            None
+        }
+    }
+}
+
 impl<'a> Module<'a> {
     pub fn new() -> Module<'a> {
-        Module {
+        unsafe { Module {
             _name: "",
             _backend: TorchBackend {},
+            _buffers: HashMap::new(),
             _params: HashMap::new(),
-            //_modulesp: LinkedHashMap::new(),
-            _modulesp: HashMap::new(),
+            _modules: HashMap::new(),
             training: true,
         }
+    }
     }
     #[inline]
     fn as_mut_ptr(&mut self) -> *mut Module<'a> {
@@ -63,14 +80,20 @@ impl<'a> Module<'a> {
     }
     pub fn add_module(&mut self, module: &mut ModIntf<'a>) {
         let m = module.delegate();
-        self._modulesp.insert(m._name, m.as_mut_ptr());
+        self._modules.insert(m._name, m.as_mut_ptr());
 
     }
     pub fn modules_iter_mut(&mut self) -> PtrIterMut<Module<'a>> {
-        PtrIterMut { mod_iter: self._modulesp.iter_mut() }
+        PtrIterMut { mod_iter: self._modules.iter_mut() }
+    }
+    pub fn modules_iter(&mut self) -> PtrIter<Module<'a>> {
+        PtrIter { mod_iter: self._modules.iter() }
     }
     pub fn params_iter_mut(&mut self) -> PtrIterMut<Parameter<'a>> {
         PtrIterMut { mod_iter: self._params.iter_mut() }
+    }
+    pub fn register_buffer(&mut self, name: &'a str, tensor: &'a mut Tensor) {
+        self._buffers.insert(name, tensor);
     }
     fn _apply(&mut self, callback: fn(&mut Tensor)) {
         for (_, module) in self.modules_iter_mut() {
@@ -92,6 +115,16 @@ impl<'a> Module<'a> {
             module.apply(callback)
         }
         callback(self)
+    }
+    pub fn repr(&mut self) -> String {
+        let mut tmpstr = format!("{} (\n", self._name );
+        for (key, module) in self.modules_iter_mut() {
+            let modstr = module.repr();
+            let modstr = format!("  {}", modstr);
+            tmpstr = format!("{} ({}): {}\n", tmpstr, key, modstr);
+        }
+        tmpstr = format!("{})", tmpstr);
+        tmpstr
     }
     pub fn eval(&mut self) {
         self.train(false)
