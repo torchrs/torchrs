df21e8564b99b64d08e760e95ace7f0c51be12ef
Matt Macy <mmacy@mattmacy.io>
2017-05-23 16:51:36 -0700

    accept Options and #ignore

--- modparse_derive/src/lib.rs
+++ modparse_derive/src/lib.rs
@@ -11,13 +11,57 @@ extern crate quote;
 
 use proc_macro::TokenStream;
 
+enum Kind {
+    Module,
+    Parameter,
+    ModIntf,
+}
 
-#[proc_macro_derive(ModParse, attributes(module))]
+#[proc_macro_derive(ModParse, attributes(ignore))]
 pub fn parse(input: TokenStream) -> TokenStream {
     let source = input.to_string();
     let mut ast = syn::parse_derive_input(&source).expect("failed to parse rust syntax");
     let gen = impl_parse(&mut ast);
     gen.parse().expect("failed to serialize into rust syntax")
+}
+fn is_type(ty: &syn::Ty, name: & 'static str) -> bool {
+    if let syn::Ty::Path(_, ref path) = *ty {
+        if path.segments.iter().any(|seg| seg.ident.as_ref() == name) { 
+            true  
+        } else {
+            false
+        }
+    } else {false}
+}
+
+fn option_type(ty: &syn::Ty) -> &syn::Ty {
+    if let syn::Ty::Path(_, ref path) = *ty {
+        if let Some(seg) = 
+            path.segments.iter().find(|seg| seg.ident.as_ref() == "Option") {
+            if let syn::PathParameters::AngleBracketed(ref data) = seg.parameters {
+            // assume simple nesting for now
+                &data.types[0]
+            } else {
+                panic!("can't parse Option segment parameters {:?}", seg.parameters);
+            }
+        } else {
+            panic!("Option not found!")
+        } 
+    } else {
+        panic!("not path")
+    }
+}
+
+fn get_type(ty: &syn::Ty) -> Kind {
+        if is_type(&ty, "Option") {
+            get_type(option_type(ty))
+        } else if is_type(&ty, "Module") {
+            Kind::Module
+        } else if is_type(&ty, "Parameter"){
+            Kind::Parameter
+        } else {
+            Kind::ModIntf
+        }
 }
 
 fn impl_parse(ast: &mut syn::DeriveInput) -> quote::Tokens {
@@ -32,17 +76,31 @@ fn impl_parse(ast: &mut syn::DeriveInput) -> quote::Tokens {
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
     let atmatch = variants.iter()
         .filter_map(|field| {
+            if field.attrs.iter().any(|attr| attr.name() == "ignore") ||
+                is_type(& field.ty, "Module")
+            { return None;}
         	let field_name = field.ident.as_ref().clone();
-
-        	let ret = if field.attrs.iter().any(|attr| attr.name() == "param") {
-        		Some( quote! { self.delegate._params.push(stringify!(#field_name) ) } )
-        	} else if field.attrs.iter().any(|attr| attr.name() == "module") {
-//        		Some ( quote! { self.delegate._modules.insert(stringify!(#field_name).to_owned(), self. #field_name .delegate()) } )
-        		Some ( quote! { 
-        			self.delegate.add_module(&mut self. #field_name );
-        		 } )
-        	} else { None };
-    		ret
+            let is_option = is_type(&field.ty, "Option");
+            get_type(&field.ty);
+            match get_type(&field.ty) {
+                Kind::Parameter if is_option => 
+                    Some( quote! { 
+                        if let Some(ref mut param) = self. #field_name {
+                            self.delegate.add_param(stringify!(#field_name), param);
+                        };
+                    } ),
+                Kind::Parameter => 
+                    Some( quote! { self.delegate.add_param(stringify!(#field_name), &mut self. #field_name)  } ),
+                Kind::ModIntf if is_option => 
+                Some ( quote! {
+                    if let Some(ref module) = self. #field_name {
+                        self.delegate.add_module(&mut module ); 
+                    };
+                } ),
+                Kind::ModIntf => 
+                    Some ( quote! { self.delegate.add_module(&mut self. #field_name ); } ),
+                _ => panic!("bad match {:?}", field.ty),
+        	}
         });
     let foo = quote! {
         impl #impl_generics ModuleStruct<'a> for #name  #ty_generics  #where_clause {
