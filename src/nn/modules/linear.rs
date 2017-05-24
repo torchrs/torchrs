72aec9c3cbeb7db2637b43723f2dcfbf3b953234
Matt Macy <mmacy@mattmacy.io>
2017-05-23 18:47:17 -0700

    first cut at builder pattern

--- src/nn/modules/linear.rs
+++ src/nn/modules/linear.rs
@@ -1,21 +1,50 @@
 use nn::modules::module::*;
+use nn::parameter::Parameter;
 
 #[derive(ModParse)]
 pub struct Linear<'a> {
     delegate: Module<'a>,
+    #[ignore]
     in_features: u32,
+    #[ignore]
+    out_features: u32,
+    weight: Parameter<'a>,
+    bias: Option<Parameter<'a>>
 }
 
 impl<'a> Linear<'a> {
-    pub fn new() -> Linear<'a> {
+	pub fn build(in_features: u32, out_features: u32) -> LinearArgsBuilder {
+		LinearArgsBuilder::default()
+			.in_features(in_features)
+			.out_features(out_features)
+	}
+    pub fn new(args: LinearArgs) -> Linear<'a> {
         let mut t = Linear {
             delegate: Module::new(),
-            in_features: 0,
+            in_features: args.in_features,
+            out_features: args.out_features,
+            weight: Parameter::default(),
+            bias: None,
         };
         t.init_module();
         t
     }
 }
+#[builder(pattern="owned")]
+#[derive(Builder)]
+pub struct LinearArgs {
+    in_features: u32,
+    out_features: u32,
+    #[builder(default="false")]
+    bias: bool
+}
+impl LinearArgsBuilder {
+	pub fn done<'a>(self) -> Linear<'a> {
+		let args = self.build().unwrap();
+		Linear::new(args)
+	}
+}
+
 
 impl<'a> ModIntf<'a> for Linear<'a> {
     fn delegate(&mut self) -> &mut Module<'a> {
