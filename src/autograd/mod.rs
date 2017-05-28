2c61d36cfdc477537d421a0214b13731a757d2ac
Matt Macy <mmacy@mattmacy.io>
2017-05-27 22:09:56 -0700

    add macro to implement FuncDelegate

--- src/autograd/mod.rs
+++ src/autograd/mod.rs
@@ -1,7 +1,10 @@
 pub mod variable;
 pub mod function;
+#[macro_use]
+pub mod macros;
 pub mod functions;
 
 pub use self::variable::*;
 pub use self::function::*;
+pub use self::macros::*;
 pub use self::functions::*;
