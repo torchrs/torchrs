4511e08175efde4a9e99363effd89289ce0a06ad
Matt Macy <mmacy@mattmacy.io>
2017-05-31 21:07:39 -0700

    sort out engine argument types

--- src/autograd/engine.rs
+++ src/autograd/engine.rs
@@ -1,11 +1,34 @@
 
-use autograd::Variable;
+use autograd::{Variable, Function, FuncId};
 use tensor::Tensor;
+use std::collections::{HashSet, HashMap};
 
 #[derive(Default, Clone)]
 pub struct ExecutionEngine {}
 
+type FnRefs = HashMap<Function, u32>;
+type FnDependencies = HashMap<Function, FnRefs>;
+
 impl ExecutionEngine {
+    fn _compute_dependencies(function: FuncId) -> FnDependencies {
+        unimplemented!()
+    }
+    fn _free_backward_dependency(dependencies: FnDependencies,
+                                 prev_fn: FuncId,
+                                 fn_: FuncId,
+                                 arg_id: u32)
+                                 -> usize {
+        unimplemented!();
+    }
+    fn _is_ready_for_backward(dependencies: FnDependencies, function: FuncId) -> bool {
+        unimplemented!()
+    }
+    fn _add_grad<T>(need_copy: HashSet<Function>,
+                    prev_grad: HashMap<u32, &Tensor<T>>,
+                    output_nr: u32,
+                    d_prev_grad: &Tensor<T>) {
+        unimplemented!()
+    }
     pub fn run_backward<T>(arg: &mut Variable<T>,
                            gradient: &mut Tensor<T>,
                            retain_variables: bool) {
