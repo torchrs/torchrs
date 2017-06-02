efd1e6ceacbc1230d2bdaba96f3f39848739870d
Matt Macy <mmacy@mattmacy.io>
2017-06-01 18:10:54 -0700

    partially implement run_backward

--- src/autograd/engine.rs
+++ src/autograd/engine.rs
@@ -1,16 +1,22 @@
 
-use autograd::{Variable, Function, FuncId};
-use tensor::Tensor;
-use std::collections::{HashSet, HashMap};
+// silence warnings while still a WIP
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
 
-#[derive(Default, Clone)]
+use autograd::{Variable, Function, FuncId, FuncIntf};
+use tensor::{Tensor, TensorKind};
+use std::collections::{HashSet, HashMap, VecDeque};
+
 pub struct ExecutionEngine {}
+
+
 
 type FnRefs = HashMap<Function, u32>;
 type FnDependencies = HashMap<Function, FnRefs>;
 
 impl ExecutionEngine {
-    fn _compute_dependencies(function: FuncId) -> FnDependencies {
+    fn _compute_dependencies(function: &Function) -> FnDependencies {
         unimplemented!()
     }
     fn _free_backward_dependency(dependencies: FnDependencies,
@@ -29,9 +35,24 @@ impl ExecutionEngine {
                     d_prev_grad: &Tensor<T>) {
         unimplemented!()
     }
-    pub fn run_backward<T>(arg: &mut Variable<T>,
-                           gradient: &mut Tensor<T>,
-                           retain_variables: bool) {
-        unimplemented!()
+    pub fn run_backward<T: Copy>(var: &mut Variable<T>, grad: TensorKind, retain_variables: bool) {
+        let grad_fn;
+        match var.grad_fn() {
+            Some(v) => grad_fn = v,
+            None => {
+                let grad_tensor = Tensor::<T>::from(grad);
+                var._do_backward(grad_tensor[0]);
+                return;
+            }
+        }
+        let mut ready = VecDeque::new();
+        ready.push_back((grad_fn.clone(), vec![grad]));
+        //let mut need_copy = HashSet::new();
+
+        let dependencies = Self::_compute_dependencies(&grad_fn);
+        while !ready.is_empty() {
+            let (func, grad) = ready.pop_front().unwrap();
+            let grad_input = func.clone()._do_backward(&grad, retain_variables);
+        }
     }
 }
