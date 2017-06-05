671cf7a1c47af938630a401246ead3a189e01b59
Matt Macy <mmacy@mattmacy.io>
2017-06-04 19:21:39 -0700

    add forward for linear

--- src/nn/_functions/thnn/linear.rs
+++ src/nn/_functions/thnn/linear.rs
@@ -1,15 +1,22 @@
-use autograd::{Function, FuncIntf, FuncDelegate, Variable, FIWrap};
-use macros::*;
-use tensor::TensorKindList;
-use ::*;
+use autograd::{Function, FuncIntf, FuncDelegate, FIWrap};
+use tensor::*;
 
 impl_func!(LinearF);
 
 impl FuncIntf for LinearF {
-    fn forward(&mut self, mut input: &mut TensorKindList) -> TensorKindList {
-        unimplemented!()
+    fn forward(&mut self, input_list: &mut TensorKindList) -> TensorKindList {
+        self.save_for_backward(input_list);
+        let (input, weight) = (input_list.remove(0), input_list.remove(0));
+        let mut output = input.new_([input.size()[0], weight.size()[0]]);
+        output = output.addmm_(&0.into(), &1.into(), &input, &weight.t());
+        if input_list.len() != 0 {
+            let bias = input_list.remove(0);
+            output = output.clone().addt_(&1.into(), &bias.expand_as(&output));
+        }
+
+        vec![output]
     }
-    fn backward(&mut self, mut input: &mut TensorKindList) -> TensorKindList {
+    fn backward(&mut self, input: &mut TensorKindList) -> TensorKindList {
         unimplemented!()
     }
 }
