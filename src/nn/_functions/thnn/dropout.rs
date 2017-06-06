43f89a255b350e50825797ae86fc16d22e4271f9
Matt Macy <mmacy@mattmacy.io>
2017-06-06 16:39:39 -0700

    implement forward/backward for dropout

--- src/nn/_functions/thnn/dropout.rs
+++ src/nn/_functions/thnn/dropout.rs
@@ -1,5 +1,6 @@
-use autograd::{Function, FuncIntf, FuncDelegate, Variable, FIWrap};
-use tensor::{TensorKindList, OptTensorKindList};
+#![allow(unused_variables)]
+use autograd::{Function, FuncIntf, FuncDelegate, FIWrap};
+use tensor::{TensorKindList, OptTensorKindList, TensorKind, New};
 
 #[builder(pattern="owned")]
 #[derive(Builder, Clone)]
@@ -21,20 +22,66 @@ impl Default for DropoutArgs {
 impl_func_args!(Dropout1d, DropoutArgs);
 impl_func_args!(Dropout2d, DropoutArgs);
 
-impl FuncIntf for Dropout1d {
-    fn forward(&mut self, mut input: &mut TensorKindList) -> TensorKindList {
-        unimplemented!()
-    }
-    fn backward(&mut self, mut input: &mut OptTensorKindList) -> OptTensorKindList {
-        unimplemented!()
+trait Noise: FuncIntf {
+    fn make_noise(&self, input: &TensorKind) -> TensorKind;
+}
+
+trait Dropout: Noise + FuncIntf {
+    fn dropout_forward(&mut self,
+                       input: &mut TensorKindList,
+                       args: &DropoutArgs)
+                       -> TensorKindList {
+        let output = if args.inplace {
+            self.mark_dirty(input);
+            input[0].clone()
+        } else {
+            input[0].copy()
+        };
+        let output = if args.p > 0. && args.training {
+            let noise = self.make_noise(&input[0])
+                .bernoulli_(&(1. - args.p).into())
+                .div_(&(1. - args.p).into())
+                .expand_as(&input[0]);
+            self.save_for_backward(&vec![noise.clone()]);
+            output.mult_(&noise)
+        } else {
+            output
+        };
+        vec![output]
     }
 }
 
-impl FuncIntf for Dropout2d {
-    fn forward(&mut self, mut input: &mut TensorKindList) -> TensorKindList {
+impl Dropout for Dropout1d {}
+impl Dropout for Dropout2d {}
+impl Noise for Dropout1d {
+    fn make_noise(&self, input: &TensorKind) -> TensorKind {
+        input.new_(())
+    }
+}
+impl Noise for Dropout2d {
+    fn make_noise(&self, input: &TensorKind) -> TensorKind {
+        let mut v = vec![input.size()[0], input.size()[1]];
+        for _ in 0..input.dim() - 2 {
+            v.push(1)
+        }
+        input.new_(v)
+    }
+}
+impl FuncIntf for Dropout1d {
+    fn forward(&mut self, input: &mut TensorKindList) -> TensorKindList {
+        let args = self.args.clone();
+        self.dropout_forward(input, &args)
+    }
+    fn backward(&mut self, input: &mut OptTensorKindList) -> OptTensorKindList {
         unimplemented!()
     }
-    fn backward(&mut self, mut input: &mut OptTensorKindList) -> OptTensorKindList {
+}
+impl FuncIntf for Dropout2d {
+    fn forward(&mut self, input: &mut TensorKindList) -> TensorKindList {
+        let args = self.args.clone();
+        self.dropout_forward(input, &args)
+    }
+    fn backward(&mut self, input: &mut OptTensorKindList) -> OptTensorKindList {
         unimplemented!()
     }
 }
