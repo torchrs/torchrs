bbe1115dd5241467e5766089c1014bf98aa39a1c
Matt Macy <mmacy@mattmacy.io>
2017-06-06 20:37:45 -0700

    implement forward threshold

--- src/nn/_functions/thnn/activation.rs
+++ src/nn/_functions/thnn/activation.rs
@@ -1,7 +1,5 @@
-use autograd::{Function, FuncIntf, FuncDelegate, Variable, VarKindList, FIWrap};
-use macros::*;
-use tensor::{TensorKindList, OptTensorKindList};
-use ::*;
+use autograd::{Function, FuncIntf, FuncDelegate, FIWrap};
+use tensor::{TensorKindList, OptTensorKindList, New};
 
 pub struct Threshold {
     delegate: Function,
@@ -12,6 +10,13 @@ pub struct Threshold {
 
 impl Threshold {
     pub fn new(threshold: f32, value: f32, inplace: bool) -> FIWrap<Self> {
+        if inplace {
+            panic!("in-place processing requires value ({}) to not \
+                    exceed threshold ({})",
+                   value,
+                   threshold);
+        }
+
         FIWrap::new(Threshold {
                         delegate: Function::new(),
                         threshold: threshold,
@@ -24,8 +29,21 @@ impl Threshold {
 impl_func_delegate!(Threshold);
 
 impl FuncIntf for Threshold {
-    fn forward(&mut self, input: &mut TensorKindList) -> TensorKindList {
-        unimplemented!()
+    fn forward(&mut self, input_: &mut TensorKindList) -> TensorKindList {
+        let backend = input_[0].backend();
+        let mut output = if self.inplace {
+            self.mark_dirty(input_);
+            input_[0].clone()
+        } else {
+            input_[0].new_(())
+        };
+        self.save_for_backward(input_);
+        backend.Threshold_updateOutput(&mut input_[0],
+                                       &mut output,
+                                       self.threshold,
+                                       self.value,
+                                       self.inplace);
+        vec![output]
     }
     fn backward(&mut self, input: &mut OptTensorKindList) -> OptTensorKindList {
         unimplemented!()
