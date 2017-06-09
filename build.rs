1cf1ab31cc22b6d2a7ca6d90a904f87ed9a626aa
Matt Macy <mmacy@mattmacy.io>
2017-06-08 21:31:21 -0700

    fmt

--- build.rs
+++ build.rs
@@ -2,9 +2,10 @@ use std::process::Command;
 
 
 fn main() {
-	let output = Command::new("python")
-			.arg(format!("{}/scripts/generate_wrappers.py", env!("CARGO_MANIFEST_DIR")))
-			.spawn()
-			.expect("failed to execute process");
-	println!("{:?}", output)
-} 
+    let output = Command::new("python")
+        .arg(format!("{}/scripts/generate_wrappers.py",
+                     env!("CARGO_MANIFEST_DIR")))
+        .spawn()
+        .expect("failed to execute process");
+    println!("{:?}", output)
+}
