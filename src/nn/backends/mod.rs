08117ed55f3b18b2061c434daf2c9bf3c9345011
Matt Macy <mmacy@mattmacy.io>
2017-06-08 17:21:34 -0700

    switch to auto-generated code

--- src/nn/backends/mod.rs
+++ src/nn/backends/mod.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 pub mod backend;
-pub mod thnn;
+pub mod thnn_float;
+pub mod thnn_double;
 
 pub use self::backend::*;
-pub use self::thnn::*;
+pub use self::thnn_float::*;
+pub use self::thnn_double::*;
