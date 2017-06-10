72ada93de1abe4b2b3d9642a06424b9c2c136c8c
Matt Macy <mmacy@mattmacy.io>
2017-06-09 21:34:14 -0700

    remove reference to loss

--- src/nn/_functions/thnn/mod.rs
+++ src/nn/_functions/thnn/mod.rs
@@ -1,6 +1,5 @@
 pub mod pooling;
 pub mod convolution;
-pub mod loss;
 pub mod activation;
 pub mod dropout;
 pub mod linear;
@@ -8,7 +7,6 @@ pub mod auto;
 
 pub use self::pooling::*;
 pub use self::convolution::*;
-pub use self::loss::*;
 pub use self::activation::*;
 pub use self::dropout::*;
 pub use self::linear::*;
