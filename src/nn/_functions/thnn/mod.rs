3c71cfe52b3d0ea7586c10bbe3f2529bb660b3bf
Matt Macy <mmacy@mattmacy.io>
2017-06-09 20:56:20 -0700

    add auto generated Function classes to build

--- src/nn/_functions/thnn/mod.rs
+++ src/nn/_functions/thnn/mod.rs
@@ -4,6 +4,7 @@ pub mod loss;
 pub mod activation;
 pub mod dropout;
 pub mod linear;
+pub mod auto;
 
 pub use self::pooling::*;
 pub use self::convolution::*;
@@ -11,3 +12,4 @@ pub use self::loss::*;
 pub use self::activation::*;
 pub use self::dropout::*;
 pub use self::linear::*;
+pub use self::auto::*;
