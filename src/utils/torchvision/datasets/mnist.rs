6bc084fc3cbe95414c4e9babdbf94c8f79eb6732
Matt Macy <mmacy@mattmacy.io>
2017-06-15 21:23:37 -0700

    further progress on MNIST

--- src/utils/torchvision/datasets/mnist.rs
+++ src/utils/torchvision/datasets/mnist.rs
@@ -8,7 +8,8 @@ use flate2::{Flush, Decompress};
 use std::io::Write;
 use utils::data::{Dataset, DatasetIntf};
 use std::rc::Rc;
-use tensor::Tensor;
+use tensor::{Tensor, TensorKind};
+use torch;
 
 type Sample = (Tensor<f32>, Tensor<i64>);
 
@@ -16,16 +17,17 @@ static URLS: [&str; 4] = ["http://yann.lecun.com/exdb/mnist/train-images-idx3-ub
                           "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
                           "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
                           "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"];
-
-
-
+static RAW_FOLDER: &str = "raw";
+static PROCESSED_FOLDER: &str = "processed";
+static TRAINING_FILE: &str = "training.pt";
+static TEST_FILE: &str = "test.pt";
 
 fn download(root: &String) -> io::Result<()> {
-    let raw_path = PathBuf::from(root).join("raw");
-    let processed_path = PathBuf::from(root).join("processed");
+    let raw_path = PathBuf::from(root).join(RAW_FOLDER);
+    let processed_path = PathBuf::from(root).join(PROCESSED_FOLDER);
     // XXX how to ignore EEXIST?
     fs::create_dir(raw_path.clone())?;
-    fs::create_dir(processed_path)?;
+    fs::create_dir(processed_path.clone())?;
 
     for url in URLS.iter() {
         println!("downloading {}", url);
@@ -56,12 +58,31 @@ fn download(root: &String) -> io::Result<()> {
         file.write_all(output.as_slice())?;
     }
     println!("Proceeding");
+
+    let training_set = vec![read_image_file(raw_path.join("train-images-idx3-ubyte")),
+                            read_label_file(raw_path.join("train-labels-idx1-ubyte"))];
+    let test_set = vec![read_image_file(raw_path.join("t10k-images-idx3-ubyte")),
+                        read_label_file(raw_path.join("t10k-labels-idx1-ubyte"))];
+
+    torch::save(processed_path.join(TRAINING_FILE), &training_set)?;
+    torch::save(processed_path.join(TEST_FILE), &test_set)?;
+
     Ok(())
 }
+
+fn read_label_file(path: PathBuf) -> TensorKind {
+    unimplemented!()
+}
+fn read_image_file(path: PathBuf) -> TensorKind {
+    unimplemented!()
+}
+
 
 pub struct MNIST {
     pub root: String,
     pub train: bool,
+    pub data: Tensor<u8>,
+    pub labels: Tensor<i64>,
 //    pub transform: Option<fn(Vec<u8>)>,
 }
 
@@ -90,11 +111,21 @@ impl MNIST {
     }
     pub fn new(args: &MNISTArgs) -> Self {
         if args.download {
-            download(&args.root);
+            download(&args.root).expect("download failed");
         }
+        let processed_path = PathBuf::from(&args.root).join(PROCESSED_FOLDER);
+        let mut v = if args.train {
+            torch::load(processed_path.join(TRAINING_FILE)).expect("torch load failed")
+        } else {
+            torch::load(processed_path.join(TEST_FILE)).expect("torch load failed")
+        };
+        let (data, labels) = (v.remove(0), v.remove(0));
+
         MNIST {
             root: args.root.clone(),
             train: args.train,
+            data: data.into(),
+            labels: labels.into(),
 //            transform: args.transform,
         }
     }
