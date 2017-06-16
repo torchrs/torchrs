094100ba9d3bdb8a82d27a29a1296dfe872c78a4
Matt Macy <mmacy@mattmacy.io>
2017-06-15 20:31:28 -0700

    implement torch.save/torch.load

---
+++ src/torch/serde.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+
+use rmps::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
+
+use std::path::Path;
+use std::io;
+use std::io::{Write, Read, Error, ErrorKind};
+
+use tensor::TensorKind;
+
+pub fn save<P>(path: P, arg: &Vec<TensorKind>) -> io::Result<usize>
+    where P: AsRef<Path>
+{
+    let mut buffer = File::create(path)?;
+    let mut encoded = Vec::new();
+    arg.serialize(&mut Serializer::new(&mut encoded)).unwrap();
+    buffer.write(encoded.as_slice())
+}
+
+pub fn load<P>(path: P) -> io::Result<Vec<TensorKind>>
+    where P: AsRef<Path>
+{
+    let mut buffer = File::open(path)?;
+    let mut encoded = Vec::new();
+    let size = buffer.read_to_end(&mut encoded)?;
+    if size == 0 {
+        return Err(Error::new(ErrorKind::UnexpectedEof, "Empty File"));
+    };
+    let mut de = Deserializer::new(&encoded[..]);
+    let decoded = Deserialize::deserialize(&mut de).expect("decode failed");
+    Ok(decoded)
+
+}
