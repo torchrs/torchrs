3e89cbdea0c3069ff9c9b33e3f26295154eec532
Matt Macy <mmacy@mattmacy.io>
2017-06-17 20:58:51 -0700

    allow view to take -1

--- src/tensor/tensor_ops.rs
+++ src/tensor/tensor_ops.rs
@@ -676,7 +676,7 @@ impl<T: Copy> Tensor<T> {
     pub fn var(&self) -> f32 {
         unimplemented!()
     }
-    pub fn view(&self, dims: &[usize]) -> Self {
+    pub fn view(&self, dims: &[isize]) -> Self {
         unimplemented!()
     }
     pub fn view_as(&self, tensor: &Self) -> Self {
@@ -1370,7 +1370,7 @@ impl TensorKind {
     pub fn var(&self) -> f32 {
         unimplemented!()
     }
-    pub fn view(&self, dims: &[usize]) -> Self {
+    pub fn view(&self, dims: &[isize]) -> Self {
         unimplemented!()
     }
     pub fn view_as(&self, tensor: &Self) -> Self {
