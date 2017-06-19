5d43eb596f6bdd07976a32fb1b131348fd13d812
Matt Macy <mmacy@mattmacy.io>
2017-06-18 18:53:20 -0700

    make fields public add upcast for OptimOpts, and Required option, implement zero_grad

--- src/optim/optimizer.rs
+++ src/optim/optimizer.rs
@@ -1,15 +1,18 @@
 pub use std::collections::HashMap;
+pub use autograd::Variable;
 pub use nn::ParamIter;
+use tensor::NewSelf;
 
 pub struct Optimizer<'a, T: Copy + 'a> {
-    params: ParamIter<'a, T>,
-    defaults: HashMap<&'static str, OptimOpts>,
+    pub params: ParamIter<'a, T>,
+    pub defaults: HashMap<&'static str, OptimOpts>,
 }
 #[derive(Clone)]
 pub enum OptimOpts {
     Bool(bool),
     Int(i32),
     Float(f32),
+    Required,
 }
 impl<T: Copy> From<T> for OptimOpts {
     #[allow(unused_variables)]
@@ -33,6 +36,30 @@ impl From<bool> for OptimOpts {
     }
 }
 
+impl OptimOpts {
+    pub fn intof32(&self) -> f32 {
+        use self::OptimOpts::Float;
+        match *self {
+            Float(v) => v,
+            _ => unimplemented!(),
+        }
+    }
+    pub fn intoi32(&self) -> i32 {
+        use self::OptimOpts::Int;
+        match *self {
+            Int(v) => v,
+            _ => unimplemented!(),
+        }
+    }
+    pub fn intobool(&self) -> bool {
+        use self::OptimOpts::Bool;
+        match *self {
+            Bool(v) => v,
+            _ => unimplemented!(),
+        }
+    }
+}
+
 impl<'a, T: Copy + 'a> Optimizer<'a, T> {
     pub fn new(params: ParamIter<'a, T>, defaults: HashMap<&'static str, OptimOpts>) -> Self {
         Optimizer {
@@ -46,6 +73,20 @@ pub trait OptIntf<'a, T: Copy + 'a> {
     fn optimizer(&mut self) -> &mut Optimizer<'a, T>;
     fn zero_grad(&mut self) {
         let opt = self.optimizer();
+        let params = opt.params.clone();
+        // XXX figure out point of parameter groups
+        for p in params {
+            // XXX when would this ever be None since we allocate on lookup?
+            let mut opt_grad = p.v.grad();
+            if let Some(ref mut grad) = opt_grad.clone() {
+                if grad.is_volatile() {
+                    grad.data().clone().zero_();
+                } else {
+                    let data = grad.data();
+                    *opt_grad = Some(Variable::new(data.new(()).zero_()));
+                }
+            }
+        }
     }
     /* ignore largely unused closure arg to start */
     fn step(&mut self);
