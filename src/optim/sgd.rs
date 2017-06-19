cacc716a0006121a184ae95aa5bbd3f3fc81f5cd
Matt Macy <mmacy@mattmacy.io>
2017-06-18 18:54:12 -0700

    mark 'lr' as required

--- src/optim/sgd.rs
+++ src/optim/sgd.rs
@@ -7,7 +7,7 @@ pub struct SGD<'a, T: Copy + 'a> {
 
 impl<'a, T: Copy + 'a> SGD<'a, T> {
     pub fn new(params: ParamIter<'a, T>, defaults: HashMap<&'static str, OptimOpts>) -> Self {
-        let mut sgd_defaults = map_opt!{"momentum" => 0.0, "dampening" => 0.0,
+        let mut sgd_defaults = map_opt!{"lr" => OptimOpts::Required, "momentum" => 0.0, "dampening" => 0.0,
                  "weight_decay"=> 0.0, "nesterov"=>false};
 
         for (ref key, ref value) in defaults {
@@ -19,8 +19,16 @@ impl<'a, T: Copy + 'a> SGD<'a, T> {
 }
 
 impl<'a, T: Copy + 'a> OptIntf<'a, T> for SGD<'a, T> {
-    fn step(&mut self) {}
     fn optimizer(&mut self) -> &mut Optimizer<'a, T> {
         &mut self.optimizer
+    }
+    fn step(&mut self) {
+        let group = &self.optimizer.defaults;
+        let weight_decay = group["weight_decay"].intof32();
+        let momentum = group["momentum"].intof32();
+        let dampening = group["dampening"].intof32();
+        let nesterov = group["nesterov"].intobool();
+
+
     }
 }
