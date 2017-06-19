466c5bd29e0e022d809395c65ee2cae5d6f7e9bb
Matt Macy <mmacy@mattmacy.io>
2017-06-18 17:03:30 -0700

    fill in sgd constructor

--- src/optim/sgd.rs
+++ src/optim/sgd.rs
@@ -1,16 +1,26 @@
+use optim::*;
 
-use optim::{Optimizer, OptIntf};
 
-pub struct SGD {
-    optimizer: Optimizer,
+pub struct SGD<'a, T: Copy + 'a> {
+    optimizer: Optimizer<'a, T>,
 }
 
-impl SGD {
-    pub fn new() -> Self {
-        SGD { optimizer: Optimizer::new() }
+impl<'a, T: Copy + 'a> SGD<'a, T> {
+    pub fn new(params: ParamIter<'a, T>, defaults: HashMap<&'static str, OptimOpts>) -> Self {
+        let mut sgd_defaults = map_opt!{"momentum" => 0.0, "dampening" => 0.0,
+                 "weight_decay"=> 0.0, "nesterov"=>false};
+
+        for (ref key, ref value) in defaults {
+            let cloned = value.clone();
+            sgd_defaults.insert(key, cloned);
+        }
+        SGD { optimizer: Optimizer::new(params, sgd_defaults) }
     }
 }
 
-impl OptIntf for SGD {
+impl<'a, T: Copy + 'a> OptIntf<'a, T> for SGD<'a, T> {
     fn step(&mut self) {}
+    fn optimizer(&mut self) -> &mut Optimizer<'a, T> {
+        &mut self.optimizer
+    }
 }
