57da590a81333e8588d1d2579d65008e7f45eccd
Matt Macy <mmacy@mattmacy.io>
2017-06-19 18:56:35 -0700

    separate Tensor and TensorKind constructors

--- src/torch/tensor.rs
+++ src/torch/tensor.rs
@@ -1,6 +1,21 @@
 
-use tensor::TensorKind;
+use tensor::{Tensor, TensorKind};
 
-pub fn byte_tensor<T>(arg: Vec<T>) -> TensorKind {
+pub fn byte_tensor_kind<T>(arg: T) -> TensorKind {
+    unimplemented!()
+}
+pub fn long_tensor_kind<T>(arg: T) -> TensorKind {
+    unimplemented!()
+}
+pub fn byte_tensor<T>(arg: T) -> Tensor<u8> {
+    unimplemented!()
+}
+pub fn float_tensor<T>(arg: T) -> Tensor<f32> {
+    unimplemented!()
+}
+pub fn double_tensor<T>(arg: T) -> Tensor<f64> {
+    unimplemented!()
+}
+pub fn long_tensor<T>(arg: T) -> Tensor<i64> {
     unimplemented!()
 }
