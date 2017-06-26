82f43bc90c9eb997c29787917e5ed08396b0417e
mmacy <mmacy@torch.rs>
2017-06-25 22:38:49 -0700

    give specific types

--- src/torch/tensor.rs
+++ src/torch/tensor.rs
@@ -1,9 +1,15 @@
-
 use tensor::{Tensor, TensorKind, NumLimits};
 
-pub fn byte_tensor_kind<T>(arg: T) -> TensorKind {
+pub fn byte_tensor_kind<u8>(arg: u8) -> TensorKind {
     unimplemented!()
 }
+pub fn byte_tensor_kind<T>(arg: T) -> TensorKind where T: AsRef<Vec<u8>> {
+	unimplemented!()
+}
+pub fn byte_tensor_kind<T>(arg: T) -> TensorKind where T: AsRef<Vec<Vec<u8>>> {
+    unimplemented!()
+}
+
 pub fn float_tensor_kind<T>(arg: T) -> TensorKind {
     unimplemented!()
 }
@@ -19,9 +25,9 @@ pub fn float_tensor<T>(arg: T) -> Tensor<f32> {
 pub fn double_tensor<T>(arg: T) -> Tensor<f64> {
     unimplemented!()
 }
-pub fn long_tensor<T>(arg: T) -> Tensor<i64> {
-    unimplemented!()
+pub fn long_tensor<T>(arg: T) -> Tensor<i64>  where T: AsRef<Vec<i64>> {
+	unimplemented!()
 }
-pub fn tensor<S, D>(arg: S) -> Tensor<D> where D: NumLimits<D>{
+pub fn tensor<S, f32>(arg: S) -> Tensor<f32> where S: AsRef<Vec<Tensor<f32>>>, f32: ::tensor::tensor::NumLimits {
     unimplemented!()
 }
