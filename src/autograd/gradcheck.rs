8b46fbdb4c4a801e188af546a47dd6835452f632
Matt Macy <mmacy@mattmacy.io>
2017-07-11 15:05:58 -0700

    get_analytical_jacobian

--- src/autograd/gradcheck.rs
+++ src/autograd/gradcheck.rs
@@ -3,10 +3,12 @@ use tensor::Tensor;
 use torch;
 use itertools::zip;
 
+type Var64List = Vec<Variable<f64>>;
+
 
 pub fn get_numerical_jacobian(func: fn(&Variable<f64>) -> Variable<f64>,
                               input: &Variable<f64>,
-                              target: &Vec<Variable<f64>>,
+                              target: &Var64List,
                               eps: Option<f64>)
                               -> Vec<Tensor<f64>> {
 
@@ -47,8 +49,58 @@ pub fn get_numerical_jacobian(func: fn(&Variable<f64>) -> Variable<f64>,
             flat_tensor[i] = orig;
 
             outb.addt_(-1., &outa).div_(2. * eps);
-            d_tensor.s([i]).copy_(&outb);
+            d_tensor.s([i as isize]).copy_(&outb);
         }
     }
+    jacobian
+}
+
+pub fn zero_gradients(input: &mut Var64List) {
+    for v in input {
+        if let &mut Some(ref mut grad) = v.grad() {
+            grad.detach_();
+            grad.data().zero_();
+        }
+    }
+}
+
+pub fn iter_gradients(input: &mut Var64List) -> Vec<Option<Tensor<f64>>> {
+    input
+        .iter_mut()
+        .map(|v| if v.requires_grad() {
+                 if let &mut Some(ref mut grad) = v.grad() {
+                     Some(grad.data().clone())
+                 } else {
+                     None
+                 }
+             } else {
+                 None
+             })
+        .collect()
+
+}
+
+pub fn get_analytical_jacobian(input: &mut Var64List, output: &Variable<f64>) -> Vec<Tensor<f64>> {
+    let mut jacobian: Vec<Tensor<f64>> = input
+        .iter()
+        .map(|v| ::torch::zeros([v.numel(), output.numel()]))
+        .collect();
+    let grad_output: Tensor<f64> = torch::zeros(output.data_borrow().size());
+    let mut flat_grad_output = grad_output.view([-1]);
+
+    for i in 0..flat_grad_output.numel() {
+        flat_grad_output.zero_();
+        flat_grad_output[i] = 1.;
+        zero_gradients(input);
+        for (ref mut jacobian_x, ref d_x_) in zip(jacobian.iter_mut(), iter_gradients(input)) {
+            let d_x_: &Option<Tensor<f64>> = d_x_;
+            if let &Some(ref d_x) = d_x_ {
+                jacobian_x.s([-1, i as isize]).copy_(d_x);
+            }
+
+        }
+
+    }
+
     jacobian
 }
